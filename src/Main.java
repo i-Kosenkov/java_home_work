6af5465728581aff9286e0b8031ac5bb229a0a0e
Kosenkov <9588434@gmail.com>
2022-12-07 13:18:20 +0300

    Add task 1 in main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
+//Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input n: ");
+        if (input.hasNextFloat()) {
+            float n = input.nextFloat();
+            System.out.println("Triangular number = " + triangular(n));
+            System.out.println("Factorial number = " + factorial(n));
+        } else {
+            System.out.println("Invalid input data. Try again.");
+        }
+        input.close();
+    }
+
+    static float triangular(float n) {
+        return (1 + n) / 2 * n;
+    }
+
+    static float factorial(float n) {
+        float result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
